e6f126773ffd9da6a913c78730be1b59699f19fc
raihanmaziz <raihan441@gmail.com>
2022-06-09 19:49:46 +0700

    Adding UI
    Menambahkan UI Main Menu

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -36,6 +36,7 @@ public class PaddleController : MonoBehaviour
 
     private void MoveObject(Vector2 movement)
     {
+        Debug.Log("Paddle Speed: " + movement);
         rig.velocity = movement;
     }
 }
